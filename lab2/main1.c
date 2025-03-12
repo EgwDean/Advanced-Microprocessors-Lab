9e7b78615af775e7afc9f64f8ca2fc5a9b184616
Konstantinos Anastasopoulos <101580598+EgwDean@users.noreply.github.com>
2025-03-12 19:05:38 +0200

    Update and rename main.c to main1.c

--- lab2/main1.c
+++ lab2/main1.c
@@ -12,8 +12,8 @@ int main() {
 	
 	// Initialize the traffic light
 	PORTD.DIR |= 0b00000101; //Pins 0 and 2 of port D are outputs
-	PORTD.OUT |= 0b00000001; // Red light for pedestrians (insert breakpoint)
-	PORTD.OUT &= 0b11111011; // Green light for cars (insert breakpoint)
+	PORTD.OUT |= 0b00000001; // Red light for pedestrians
+	PORTD.OUT &= 0b11111011; // Green light for cars
 	
 	// Initialize the counter
 	TCA0.SPLIT.CTRLD = 1; // Enable split mode
@@ -31,13 +31,13 @@ int main() {
 		
 		if (x == 1) {
 			
-			x = 0; // Break out of the conditional block after this iteration (insert breakpoint)
+			x = 0; // Break out of the conditional block after this iteration
 			TCA0.SPLIT.LCNT = 0; // Clear counter
 			TCA0.SPLIT.LCMP0 = value3; // Pedestrian can use the button after the counter counts up to value3
 			int_type = 2; // Button time interrupt
 			TCA0.SPLIT.CTRLA |= 1; // Counter starts to count
 		}
-		PORTD.OUT |= 0b00000000; // NOP (insert breakpoint)
+		PORTD.OUT |= 0b00000000; // NOP (used for breakpoints)
 	}
 	cli(); // Stop accepting interrupts
 }
@@ -46,8 +46,8 @@ ISR(PORTF_PORT_vect) { // Is triggered after the press of the button
 	
 	if (allow_button == 1) { // If pedestrian is allowed to press the button
 		allow_button = 0; // Pedestrian cannot press the button anymore
-		PORTD.OUT |= 0b00000100; // Red light for cars (insert breakpoint)
-		PORTD.OUT &= 0b11111110; // Green light for pedestrians (insert breakpoint)
+		PORTD.OUT |= 0b00000100; // Red light for cars
+		PORTD.OUT &= 0b11111110; // Green light for pedestrians
 		int_type = 1; // traffic light interrupt
 		TCA0.SPLIT.CTRLA |= 1; // Counter starts to count
 	}
@@ -58,14 +58,14 @@ ISR(TCA0_LCMP0_vect) {
 	
 	if (int_type == 1) { // traffic light changes state
 		
-		PORTD.OUT |= 0b00000001; // Red light for pedestrians (insert breakpoint)
-		PORTD.OUT &= 0b11111011; // Green light for cars (insert breakpoint)
+		PORTD.OUT |= 0b00000001; // Red light for pedestrians
+		PORTD.OUT &= 0b11111011; // Green light for cars
 		x = 1; // Change flag to get out of the loop
 	}
 	
 	if (int_type == 2) {
 		
-		allow_button = 1; // Allow the press of the button (insert breakpoint)
+		allow_button = 1; // Allow the press of the button
 		int_type = 0; // None of the counter interrupts should happen
 		TCA0.SPLIT.LCNT = 0; // Clear the counter
 	}
