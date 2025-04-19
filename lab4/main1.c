2c209d2ee27ce72da156637299f95f97ba211611
Theofrastos Paximadis <161208255+Theofrastos7@users.noreply.github.com>
2025-04-19 11:29:49 +0300

    Update main1.c

--- lab4/main1.c
+++ lab4/main1.c
@@ -10,14 +10,14 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define T1 20
-#define T2 41
+#define T1 19
+#define T2 39
 
 int x = 0;
 
 int main(){
 	
-	PORTD.DIR = PIN0_bm | PIN1_bm; //PIN is output
+	PORTD.DIR |= PIN0_bm | PIN1_bm; //PIN is output
 	PORTD.OUT |= 0b00000011;
 	//prescaler=1024
 	TCA0.SPLIT.CTRLD = 1; // Enable split mode 
@@ -29,11 +29,11 @@ int main(){
 	
 	PORTF.PIN6CTRL |= PORT_PULLUPEN_bm | PORT_ISC_BOTHEDGES_gc;
 	
-    	TCA0.SPLIT.INTCTRL = TCA_SPLIT_LUNF_bm |  TCA_SPLIT_HUNF_bm;
+    TCA0.SPLIT.INTCTRL = TCA_SPLIT_LUNF_bm |  TCA_SPLIT_HUNF_bm;
 	
 	//TCA0.SPLIT.CTRLA |= TCA_SPLIT_ENABLE_bm; //Enable TCA
 		
-	sei();
+	sei(); // breakpoint
 	
 		
 	while (1){
@@ -45,17 +45,14 @@ int main(){
 		
 		TCA0.SPLIT.LPER = T1; // wings
 		TCA0.SPLIT.HPER = T2; // base		
-		
-		//TCA0.SPLIT.LCMP0 = 128; //select the duty cycle
-		//TCA0.SPLIT.HCMP0 = 128;
-		
+				
 		TCA0.SPLIT.LCMP0 = 0.6 * TCA0.SPLIT.LPER;		
 		TCA0.SPLIT.HCMP0 = 0.4 * TCA0.SPLIT.HPER;
 
 		TCA0.SPLIT.LCNT = 1;
 		TCA0.SPLIT.HCNT = 0;
 		
-		TCA0.SPLIT.CTRLA |= TCA_SPLIT_ENABLE_bm; //Enable TCA		
+		TCA0.SPLIT.CTRLA |= TCA_SPLIT_ENABLE_bm; //Enable TCA breakpoint	
 		
 	}
 	
@@ -69,7 +66,7 @@ int main(){
 		TCA0.SPLIT.LCMP0 = 0.5 * TCA0.SPLIT.LPER;
 
 		TCA0.SPLIT.LCNT = 0;
-		TCA0.SPLIT.HCNT = 1;
+		TCA0.SPLIT.HCNT = 1; // breakpoint
 		
 	}
 	
@@ -77,12 +74,15 @@ int main(){
 		
 		PORTD.OUT |= 0b00000011;
 		x = x + 1;
-		TCA0.SPLIT.CTRLA &= ~TCA_SPLIT_ENABLE_bm; //Disable TCA		
+		TCA0.SPLIT.CTRLA &= ~TCA_SPLIT_ENABLE_bm; //Disable TCA	breakpoint
 		
 	}
-				
-	PORTD.OUT |= 0b00000000; 
 		
+		
+	PORTD.OUT |= 0b00000000; //breakpoint
+	
+	
+	
 	}
 }
 
@@ -91,7 +91,7 @@ ISR(TCA0_LUNF_vect){ // every 1 ms, wings
 	PORTD.OUT ^= PIN0_bm; 
 	
 	int intflags = TCA0.SINGLE.INTFLAGS;
-	TCA0.SINGLE.INTFLAGS = intflags;
+	TCA0.SINGLE.INTFLAGS = intflags; // breakpoint
 
 }
 
@@ -100,7 +100,7 @@ ISR(TCA0_HUNF_vect){ // every 2 ms, base
 	PORTD.OUT ^= PIN1_bm; 
 
 	int intflags = TCA0.SINGLE.INTFLAGS;
-	TCA0.SINGLE.INTFLAGS = intflags;
+	TCA0.SINGLE.INTFLAGS = intflags; // breakpoint
 
 }
 
@@ -108,9 +108,9 @@ ISR(TCA0_HUNF_vect){ // every 2 ms, base
 ISR(PORTF_PORT_vect){
 	int y = PORTF.INTFLAGS; //Procedure to
 	PORTF.INTFLAGS=y; //clear the interrupt flag
-	x = x + 1; //change logic flag to get out of loop
+	x = x + 1; //change logic flag to get out of loop breakpoint
 	
 	if (x == 7){
-		x = 1;
+		x = 1; // breakpoint
 	}
 }
